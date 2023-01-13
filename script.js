d304ab3ef40b9ba75f79e42f2f4015c59efa5dd6
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-01-13 03:04:24 -0300

    JsCSS

---
+++ script.js
@@ -0,0 +1,17 @@
+let menuVisible = false;
+//Función que oculta o muestra el menú
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false;
+        
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar () {
+    document.getElementById("nav").classList="";
+    menuVisible=false;
+}
