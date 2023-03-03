0ac75badc12a379556dfb75bc7bce7a099406a50
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-03-03 10:16:52 -0300

    responsive header changes

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ function seleccionar () {
     document.getElementById("nav").classList="";
     menuVisible=false;
 }
+
+
+// function mostrarOcultarMenu() {
+//     var menu = document.querySelector('.menu');
+//     menu.classList.toggle('active');
+//   }
