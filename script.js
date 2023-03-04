c89869434d535415df251c6922398f28019f413f
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-03-04 00:26:44 -0300

    mediaquerys

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-let menuVisible = false;
-//Función que oculta o muestra el menú
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList="";
-        menuVisible = false;
+// let menuVisible = false;
+// //Función que oculta o muestra el menú
+// function mostrarOcultarMenu(){
+//     if(menuVisible){
+//         document.getElementById("nav").classList="";
+//         menuVisible = false;
         
-    }else{
-        document.getElementById("nav").classList="responsive";
-        menuVisible = true;
-    }
+//     }else{
+//         document.getElementById("nav").classList="responsive";
+//         menuVisible = true;
+//     }
+// }
+
+// function seleccionar () {
+//     document.getElementById("nav").classList="";
+//     menuVisible=false;
+// }
+
+const menu = document.querySelector(".main-menu");
+const links = document.querySelectorAll(".main-menu .main-menu__item .main-menu__link");
+const fabars = document.querySelector(".fa-bars");
+
+function handleMenu() {
+    menu.classList.toggle("menu_show");
 }
 
-function seleccionar () {
-    document.getElementById("nav").classList="";
-    menuVisible=false;
-}
+fabars.addEventListener("click", handleMenu);
+
+links.forEach((link) => {
+    link.addEventListener("click", handleMenu);
+});
+
 
 
 // function mostrarOcultarMenu() {
