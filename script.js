cb4fccfa0823fc6e402982d2929269f0915bb831
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-07-04 03:08:02 -0300

    responsive changes

--- script.js
+++ script.js
@@ -66,22 +66,22 @@ function toggleIcon() {
 }
 
 /* Cambiar idioma */
-
 const flagsElement = document.getElementById("flags");
-
 const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
-  const requestJson = await fetch(`/languages/${language}.json`);
-  const texts = await requestJson.json();
+  try {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
 
-  for (const textToChange of textsToChange) {
-    const section = textToChange.dataset.section;
-    const value = textToChange.dataset.value;
+    for (const textToChange of textsToChange) {
+      const section = textToChange.dataset.section;
+      const value = textToChange.dataset.value;
 
-    console.log(section, value);
-
-    textToChange.innerHTML = texts[section][value];
+      textToChange.innerHTML = texts[section][value];
+    }
+  } catch (error) {
+    console.error("Error al cambiar el idioma:", error);
   }
 };
 
