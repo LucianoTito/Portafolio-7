c92fed4a37394ce76070ebf4d248ce28e1af5f9e
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-07-04 02:14:59 -0300

    languages changes

--- script.js
+++ script.js
@@ -64,3 +64,27 @@ function toggleIcon() {
   darkIcon.classList.toggle("hidden");
   lightIcon.classList.toggle("hidden");
 }
+
+/* Cambiar idioma */
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`/languages/${language}.json`);
+  const texts = await requestJson.json();
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+
+    console.log(section, value);
+
+    textToChange.innerHTML = texts[section][value];
+  }
+};
+
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
